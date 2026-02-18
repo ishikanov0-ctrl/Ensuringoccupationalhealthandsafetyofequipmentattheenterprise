0f8ac153147cf885b7bb34c537544004325d72fd
ishikanov0-ctrl <ishikanov0@gmail.com>
2026-02-18 23:50:12 +0300

    Delete app directory

--- app/src/main/java/com/example/ensuringoccupationalhealthandsafetyofequipmentattheenterprise/utils/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.ensuringoccupationalhealthandsafetyofequipmentattheenterprise.utils;
-
-public class Constants {
-    public static final String ROLE_WORKER = "worker";
-    public static final String ROLE_ENGINEER = "engineer";
-    public static final String ROLE_ADMIN = "admin";
-}
